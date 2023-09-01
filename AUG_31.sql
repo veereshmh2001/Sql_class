5da642bbbc9a600a10e08d5050a7f83e56f7b441
veereshmh2001 <veereshmh.xworkz@gmail.com>
2023-09-01 15:39:47 +0530

    Nov1

--- AUG_31.sql
+++ AUG_31.sql
@@ -1,4 +1,23 @@
-CREATE DATABASE Car;
-USE Car;
+USE factory;
 
-USE xworkz;
+CREATE TABLE factory_info(f_name varchar(11),f_state varchar(15), f_city varchar(10), f_total int, f_pincode int, f_workers int, f_material varchar(20),f_hours int);
+
+SELECT * FROM factory_info;
+
+ALTER TABLE factory_info ADD COLUMN(f_start int, f_end int);
+
+CREATE DATABASE pub;
+
+CREATE TABLE pub_info(p_name varchar(10), p_area varchar(9), p_capacity int, p_start int,p_end int, p_boys int, p_girls int, p_entryfees int);
+
+select * from pub_info;
+
+ALTER TABLE pub_info ADD COLUMN(p_tickets int, p_validity varchar(5));
+
+create database camera;
+
+create table camera_info(c_brand varchar(11), c_features int, c_size int , c_lens int, c_type varchar(10),c_price int,c_discount int, c_available varchar(4));
+
+select * from camera_info;
+
+ALTER TABLE camera_info ADD COLUMN (p_bluetooth varchar(5),p_warranty varchar(5));
